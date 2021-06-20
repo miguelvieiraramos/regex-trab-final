9defcb96d2b993d2be5e81a52d3787dc4313e8b6
Miguel Ramos <miguelvieiraramos02@gmail.com>
2021-06-20 16:12:22 -0300

    fix: change starts-with-00-and-ends-with-10 to starts-with-01-and-ends-with-10

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class SentenceValidation:
     regular_expressions = {
         'even-binary': '^([01]{2})*$',
         'twice-101': '(.*101){2}',
-        'starts-with-00-and-ends-with-10': '^01[01]*10$',
+        'starts-with-01-and-ends-with-10': '^01[01]*10$',
         'any-binary-sequence': '^[01]*$',
         '0110-and-1001': '^(0110|1001)$'
     }
